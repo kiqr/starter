36824cd6740da63ca98bea759f55bfa2a2f3eb9f
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-03-29 16:54:40 +0100

    feat: added component Navbar::Item

--- app/components/navbar/component.rb
+++ app/components/navbar/component.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Navbar::Component < ApplicationViewComponent
-  renders_many :right_sections
-
   def dark_mode_switch
     render(Navbar::DarkModeSwitch::Component.new)
   end
@@ -13,5 +11,9 @@ class Navbar::Component < ApplicationViewComponent
 
   def separator
     render(Navbar::Separator::Component.new)
+  end
+
+  def item(**)
+    render(Navbar::Item::Component.new(**))
   end
 end
