165507a9a492df792e3c6f825f25c8d6caf9267b
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-04-08 23:06:08 +0200

    chore: subfolder for mailer views

--- test/mailers/previews/account_mailer_preview.rb
+++ test/mailers/previews/account_mailer_preview.rb
@@ -1,4 +1,4 @@
-# Preview all emails at http://localhost:3000/rails/mailers/kiqr/account_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/account_mailer
 class AccountMailerPreview < ActionMailer::Preview
   def invitation_email
     AccountMailer.invitation_email(AccountInvitation.first)
