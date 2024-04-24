228f2e95252bae5ff995137e8c6f422fc4286e64
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-04-24 15:24:10 +0200

    test: fix social account button names after translation

--- test/system/users/omniauth_authentication_test.rb
+++ test/system/users/omniauth_authentication_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class OmniauthAuthenticationTest < ApplicationSystemTestCase
   test "creates account if an account with the email does not exist" do
     visit new_user_session_path
-    click_on "Login with OmniAuth"
+    click_on I18n.t("partials.social_accounts_login.buttons.developer")
 
     assert_current_path "/auth/developer"
 
@@ -18,7 +18,7 @@ class OmniauthAuthenticationTest < ApplicationSystemTestCase
   test "shows message if account with the current email already exist" do
     user = create(:user)
     visit new_user_session_path
-    click_on "Login with OmniAuth"
+    click_on I18n.t("partials.social_accounts_login.buttons.developer")
     fill_in "name", with: "John Doe"
     fill_in "email", with: user.email
     click_on "Sign In"
@@ -29,7 +29,7 @@ class OmniauthAuthenticationTest < ApplicationSystemTestCase
   test "can login with an already existing connection" do
     omniauth_identity = create(:omniauth_identity)
     visit new_user_session_path
-    click_on "Login with OmniAuth"
+    click_on I18n.t("partials.social_accounts_login.buttons.developer")
     fill_in "name", with: "John Doe"
     fill_in "email", with: omniauth_identity.provider_uid
     click_on "Sign In"
