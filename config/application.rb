55fac1f3cf5f3e3cf8d248e0bd4752a465ea9ead
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-03 11:38:35 +0200

    refactor: move Kiqr.default_url_options to gem #14

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require_relative "../lib/kiqr"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
