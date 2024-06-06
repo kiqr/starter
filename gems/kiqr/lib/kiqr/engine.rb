84f32e99236d89a74aa10e46c51758454cf6d2bf
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-06 17:02:49 +0200

    Revert "feat: rails kiqr:install"
    This reverts commit 7f010c0779710a14fd9006e26946fb3ba60596da.

--- gems/kiqr/lib/kiqr/engine.rb
+++ gems/kiqr/lib/kiqr/engine.rb
@@ -2,8 +2,6 @@ require "kiqr/rails/routes"
 
 module Kiqr
   class Engine < ::Rails::Engine
-    isolate_namespace Kiqr
-
     initializer "kiqr.action_mailer" do |app|
       config.action_mailer.default_url_options = Kiqr.default_url_options
     end
