7f010c0779710a14fd9006e26946fb3ba60596da
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-06-06 17:00:44 +0200

    feat: rails kiqr:install

--- gems/kiqr/lib/kiqr/engine.rb
+++ gems/kiqr/lib/kiqr/engine.rb
@@ -2,6 +2,8 @@ require "kiqr/rails/routes"
 
 module Kiqr
   class Engine < ::Rails::Engine
+    isolate_namespace Kiqr
+
     initializer "kiqr.action_mailer" do |app|
       config.action_mailer.default_url_options = Kiqr.default_url_options
     end
