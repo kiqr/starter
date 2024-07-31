e4070d21d888bbdb7e4d2e4c4a9b8c4b8640d042
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-07-31 20:07:53 +0200

    feat: submenu for settings page

--- app/controllers/users/settings/application_controller.rb
+++ app/controllers/users/settings/application_controller.rb
@@ -1,4 +1,6 @@
 class Users::Settings::ApplicationController < ApplicationController
+  layout "settings"
+
   before_action do
     # This is to set the breadcrumbs for the onboarding process.
     add_breadcrumb I18n.t("breadcrumbs.settings"), user_settings_profile_path
