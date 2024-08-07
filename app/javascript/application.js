04f6b931bc0338303183ab0c3acab7006033b3b9
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-07 16:01:37 +0200

    chore: remove fontawesome listener

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-
-document.addEventListener("turbolinks:load", function () {
-  FontAwesome.dom.i2svg();
-});
