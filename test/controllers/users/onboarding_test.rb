8f327a7398edd6be65ac16572a2e8f93763347a0
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-17 15:59:19 +0200

    test: extract tests for onboarding to app

--- test/controllers/users/onboarding_test.rb
+++ test/controllers/users/onboarding_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Kiqr::OnboardingControllerTest < ActionDispatch::IntegrationTest
+class Users::OnboardingTest < ActionDispatch::IntegrationTest
   test "redirects to dashboard_path if user is already onboarded" do
     sign_in create(:user)
     get user_onboarding_path
@@ -16,14 +16,17 @@ class Kiqr::OnboardingControllerTest < ActionDispatch::IntegrationTest
   test "can onboard user" do
     user = create(:user, personal_account: nil)
     sign_in user
-    post user_onboarding_path, params: { user: { personal_account_attributes: { name: "Foobar zoo" } } }
+    patch user_onboarding_path, params: { user: { personal_account_attributes: { name: "Foobar zoo" } } }
     assert_redirected_to dashboard_path
-    assert user.reload.personal_account.personal?
+    user.reload
+
+    assert user.personal_account.personal?
+    assert_equal "Foobar zoo", user.personal_account.name
   end
 
   test "validates user onboarding" do
     sign_in create(:user, personal_account: nil)
-    post user_onboarding_path, params: { user: { personal_account_attributes: { name: "no" } } }
+    patch user_onboarding_path, params: { user: { personal_account_attributes: { name: "no" } } }
     assert_response :unprocessable_entity
     assert_template :new
   end
