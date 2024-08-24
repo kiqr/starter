b1cc4807cf346df6b9650fef39a203bb50e39d49
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-24 19:00:47 +0200

    refactor: setup_base_breadcrumbs with before action

--- demoapp/app/controllers/application_controller.rb
+++ demoapp/app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # Turn on request forgery protection. Bear in mind that GET and HEAD requests are not checked.
   protect_from_forgery with: :exception, prepend: true
 
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+
+  # Add more types of flash message to match the available variants in Irelia::Notification::Component.
+  add_flash_types :success, :warning
+
   # Ensure that a user is signed in and has completed the onboarding process before accessing any other page.
   # This is done in the ApplicationController to ensure that all controllers inherit this behavior.
   # To skip this behavior in a controller, use:
@@ -10,22 +16,20 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :ensure_onboarded
 
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  # Add more types of flash message to match the available variants in Irelia::Notification::Component.
-  add_flash_types :success, :warning
-
   # Configure the base breadcrumbs for all pages to "Dashboard > (Account Name)"
-  before_action do
-    add_breadcrumb helpers.irelia_icon { "fa fa-home" }, (user_signed_in? ? dashboard_path : root_path)
-    add_breadcrumb current_account.name, dashboard_path if current_account.present?
-  end
+  before_action :setup_base_breadcrumbs
 
   # Automatically include account_id in all URL options if it is already present in the params.
   # This is used to ensure that all routes are scoped to the current team. Personal account
   # routes are not affected.
   def default_url_options
     { account_id: params[:account_id] }
+  end
+
+  protected
+
+  def setup_base_breadcrumbs
+    add_breadcrumb helpers.irelia_icon { "fa fa-home" }, (user_signed_in? ? dashboard_path : root_path)
+    add_breadcrumb current_account.name, dashboard_path if current_account.present?
   end
 end
