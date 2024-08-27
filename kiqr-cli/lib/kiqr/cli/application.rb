1fa24b0aa090691b37e97d507352c9b56e803558
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-28 00:30:14 +0200

    app generator creates fresh ruby application

--- kiqr-cli/lib/kiqr/cli/application.rb
+++ kiqr-cli/lib/kiqr/cli/application.rb
@@ -9,6 +9,14 @@ module Kiqr
         true
       end
 
+      desc "new", "Create a new KIQR application"
+      argument :app_name, type: :string, required: true, desc: "The name of the new application"
+      def new
+        puts "Argument directory: #{app_name}"
+        require "kiqr/cli/extensions/generators/app/app_generator"
+        invoke Kiqr::Cli::Extensions::Generators::AppGenerator, [app_name]
+      end
+
       desc "extensions", "Create or manage extensions"
       subcommand "extensions", Kiqr::Cli::Extensions::Commands
 
