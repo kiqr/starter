04c804cd459a028ab93993d480aed3899a6f38fd
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-31 00:36:04 +0200

    style: rubocop linting

--- kiqr-core/lib/kiqr/cli/generators/dummy/dummy_generator.rb
+++ kiqr-core/lib/kiqr/cli/generators/dummy/dummy_generator.rb
@@ -43,7 +43,7 @@ module Kiqr
               remove_file file
             end
           end
-        end 
+        end
 
         protected
 
--- kiqr-core/lib/kiqr/frontend/theme_config.rb
+++ kiqr-core/lib/kiqr/frontend/theme_config.rb
@@ -6,7 +6,7 @@ module Kiqr
       # ==> View path
       # Specifies the directory path where the theme's view templates are located.
       # This allows customization of the theme's view rendering location.
-      config_accessor :view_path 
+      config_accessor :view_path
     end
   end
 end
