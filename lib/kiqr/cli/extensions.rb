f2d2dbe2b8559465c9d80fd9493dae7b883ab78c
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-31 12:48:29 +0200

    remove old dummy generator

--- lib/kiqr/cli/extensions.rb
+++ lib/kiqr/cli/extensions.rb
@@ -6,11 +6,6 @@ module Kiqr
       def self.exit_on_failure?
         true
       end
-
-      desc "test_app", "Generate a dummy application for testing"
-      def test_app
-        Kiqr::Cli::Generators::DummyGenerator.start
-      end
     end
   end
 end
