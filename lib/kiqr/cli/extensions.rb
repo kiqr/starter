6f6022a960a684dcd2fde847e274c42e58a5e80a
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-31 13:44:47 +0200

    test if it runs

--- lib/kiqr/cli/extensions.rb
+++ lib/kiqr/cli/extensions.rb
@@ -6,6 +6,11 @@ module Kiqr
       def self.exit_on_failure?
         true
       end
+
+      desc "test_app", "Create a dummy app for testing"
+      def test_app
+        Kiqr::Cli::Generators::DummyGenerator.start
+      end
     end
   end
 end
---
+++ lib/kiqr/cli/generators/dummy/dummy_generator.rb
@@ -0,0 +1,55 @@
+require "rails/generators/rails/app/app_generator"
+require "fileutils"
+
+module Kiqr
+  module Cli
+    module Generators
+      class DummyGenerator < Rails::Generators::Base
+        source_paths << File.expand_path("templates", __dir__)
+
+        def clean_up_dummy_directory
+          say "Cleaning up dummy directory: #{dummy_path}"
+          remove_dir dummy_path
+        end
+
+        def create_dummy_app
+          opts = {
+            skip_bundle: true,
+            skip_git: true,
+            skip_listen: true,
+            skip_rc: true,
+            skip_spring: true,
+            skip_test: true,
+            skip_bootsnap: true
+          }
+
+          say "Generating dummy Rails application at #{dummy_path}"
+          invoke Rails::Generators::AppGenerator, [ dummy_path ], opts
+        end
+
+        private
+
+        def dummy_path
+          @dummy_path ||= File.join(extension_root_dir, "test/dummy")
+        end
+
+        # Recursively search for the folder containing the gemspec file.
+        def extension_root_dir
+          find_upwards("*.gemspec")
+        end
+
+        # Allows wildcard search for file name.
+        def find_upwards(file)
+          current_path = Dir.pwd
+          while current_path != "/"
+            if Dir.glob(File.join(current_path, file)).any?
+              return current_path
+            else
+              current_path = File.expand_path(File.join(current_path, ".."))
+            end
+          end
+        end
+      end
+    end
+  end
+end
