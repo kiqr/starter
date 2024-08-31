7e27f147c203cbce3bc3e1c2528dbe4d194ea370
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-08-31 16:28:35 +0200

    with working tests

--- lib/generators/kiqr/install/install_generator.rb
+++ lib/generators/kiqr/install/install_generator.rb
@@ -12,5 +12,32 @@ module Kiqr
     def override_app_files
       directory "rails", Rails.root, force: true
     end
+
+    def write_kiqr_routes_to_routes_file
+      routes_addition = <<~EOS
+      root "public#landing_page"
+
+        # => KIQR core routes
+        # These routes are required for the KIQR core to function properly.
+        # Refer to the KIQR documentation for more information on how
+        # to customize these routes or override controllers.
+        kiqr_routes
+
+        # Routes inside this block will be prefixed with /team/<team_id> if
+        # the user is signed in to a team account. Otherwise, they won't be prefixed at all.
+        #
+        # Example:
+        # /team/:team_id/dashboard <- if user is signed in to a team account
+        # /dashboard <- if user is browsing the app without a team account
+        #
+        account_scope do
+          get "dashboard", to: "dashboard#show"
+        end
+      EOS
+
+      gsub_file File.expand_path("config/routes.rb", Rails.root), /\# root "posts#index"/ do
+        routes_addition.strip
+      end
+    end
   end
 end
